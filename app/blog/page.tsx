c7690ca1294f83ec5301c7356d4fe1670d790119
rubisanto <antoine.gogce@capgemini.com>
2023-08-09 15:59:46 +0200

    json-server + datafetching init

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,24 @@
-export default function Blog() {
-  return <p>page blog </p>;
+type Article = {
+  id: number;
+  title: string;
+  content: string;
+};
+
+export default async function Blog() {
+  // articles du blog
+  const res = await fetch("http://localhost:4000/blog");
+  const blog: Article[] = await res.json();
+  return (
+    <div>
+      <p>page blog </p>
+      <ul>
+        {blog.map((article) => (
+          <li key={article.id}>
+            <h3>{article.title}</h3>
+            <p>{article.content}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
